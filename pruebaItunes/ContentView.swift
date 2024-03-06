3cf6ec727ed320d7d8f84e06d52ab2871c0ae480
andrea4990 <andreavr04@gmail.com>
2024-03-06 16:59:35 -0300

    Initial Commit

---
+++ pruebaItunes/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  pruebaItunes
+//
+//  Created by Andrea Vera on 06-03-24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ pruebaItunes/pruebaItunesApp.swift
@@ -0,0 +1,17 @@
+//
+//  pruebaItunesApp.swift
+//  pruebaItunes
+//
+//  Created by Andrea Vera on 06-03-24.
+//
+
+import SwiftUI
+
+@main
+struct pruebaItunesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
